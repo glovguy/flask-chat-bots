8ea252766217af31188d02214aa99fd8ff5f74d0
Karl <karlsmith@bouzou.com>
2017-07-12 23:09:07 -0400

    Fixes some formatting issues

--- bots/rails_client.py
+++ bots/rails_client.py
@@ -5,13 +5,12 @@ class RailsClient(object):
     def __init__(self):
         self.url = "http://localhost:3000/api/messages"
 
-    @classmethod
-    def send_message(cls, message_text):
-        r = cls.post_message(message_text)
+    def send_message(self, message_text):
+        r = self.post_message(message_text)
         print("posted!")
         print(r.status_code, r.reason)
 
-    def post_message(message_text):
+    def post_message(self, message_text):
         msg_json={ 'data': { 'attributes': { 'body': '', 'sender': 2, 'style': 'bot' } } }
         msg_json['data']['attributes']['body'] = message_text
         return requests.post(self.url, json=msg_json)
--- tasks.py
+++ tasks.py
@@ -7,7 +7,8 @@ celery_app = Celery('tasks', broker='redis://localhost:6379/0')
 @celery_app.task
 def incoming_message(data):
     print("Message received, data: ", data)
-    if data.get('style') is ['user']:
+    if data.get('style') == ['user']:
+        msg_client = RailsClient()
         time.sleep(3)
-        send_message('Hello from the bot inside your computer')
-    return 'some message here'
+        msg_client.send_message('Hello from the bot inside your computer')
+    return "Message received, data: {0}".format(data)
