99f4388398c923c0b8b93925c5f1c9902361b666
Karl <karlsmith@bouzou.com>
2017-09-11 20:40:02 -0400

    Adds wordnet comparison to double check, adds reply for if object is not a physical entity

--- tasks.py
+++ tasks.py
@@ -2,6 +2,7 @@ import time
 import os
 from celery import Celery
 import spacy
+from nltk.corpus import wordnet
 from bots.chat_client import ChatClient
 
 celery_app = Celery('tasks', broker=os.environ['REDIS_URL'])
@@ -25,15 +26,38 @@ def sandwich_bot_feed(data):
     chat_client = ChatClient(data)
     if chat_client.last_sender() != 'user':
         return "Not replying, since message is not from user."
-    word = nlp(chat_client.last_message())[0]
-    print("Word: {0}".format(word))
-    sandw_similarity = word.similarity(SANDWICH)
-    print("Sandwich similarity: {0}".format(sandw_similarity))
-    if sandw_similarity > 0.5:
-        reply_msg = "Yes, that's a sandwich"
+
+    comparison_text = chat_client.last_message()
+    comparison_phrase = nlp(comparison_text)
+    print("Phrase: {0}".format(comparison_phrase))
+    sandw_similarity = comparison_phrase.similarity(SANDWICH)
+    print("Sandwich word vector similarity: {0}".format(sandw_similarity))
+
+    words = wordnet.synsets(comparison_text)
+    if len(words) > 0:
+        word = words[0]
+        lowest_c_hypernyms = WN_SANDWICH.lowest_common_hypernyms(word)
+        if len(lowest_c_hypernyms) > 0:
+            lowest_hyper = lowest_c_hypernyms[0]
+        else:
+            lowest_hyper = None
     else:
-        reply_msg = "No, that's not a sandwich"
+        word = None
+        lowest_hyper = None
+    print("Synset matched: {0}".format(word))
+    print("Lowest common hypernym: {0}".format(lowest_hyper))
+
+    if sandw_similarity > 0.5 or lowest_hyper is WN_SANDWICH:
+        reply_msg = "yes, that's a sandwich"
+    elif lowest_hyper is WN_ENTITY:
+        reply_msg = "...that's not even a physical object"
+    elif sandw_similarity == 0:
+        reply_msg = "I don't know what that is"
+    else:
+        reply_msg = "no, that's not a sandwich"
     chat_client.send_message(reply_msg)
     return "Replying with: {0}".format(reply_msg)
 
-SANDWICH = nlp(u'sandwich')[0]
+SANDWICH = nlp(u'sandwich')
+WN_SANDWICH = wordnet.synset('sandwich.n.01')
+WN_ENTITY = wordnet.synset('entity.n.01')
