e66a02d8a503faa2bf7f926cf52526b043402875
Karl <karlsmith@bouzou.com>
2017-10-11 20:57:35 -0400

    Changes app run details for heroku

--- main.py
+++ main.py
@@ -36,5 +36,5 @@ MESSAGE_RECEIVED = {
 
 if __name__ == '__main__':
     app.debug = True
-    # port = int(os.environ.get("PORT", 5005))
-    app.run()
+    port = int(os.environ.get("PORT", 5005))
+    app.run(host='0.0.0.0', port=port)
