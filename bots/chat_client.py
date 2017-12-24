fa31ccc8e35ce741f7884ce71effbb23f77b2d2e
Karl <karlsmith@bouzou.com>
2017-12-23 19:48:06 -0600

    Adds ignore option to ChatClient

--- bots/chat_client.py
+++ bots/chat_client.py
@@ -13,13 +13,16 @@ class ChatClient(object):
         return self.data.get('body')[0]
 
     def send_message(self, message_text):
+        if self.data.get('ignore'):
+            print("Ignoring this message.")
+            return
         r = self.post_message(message_text)
         if r.status_code != 200:
             print("Error sending message to chat server")
             print(str(r.status_code) + ' ' + str(r.reason))
 
     def post_message(self, message_text):
-        msg_json={ 'data': { 'attributes': { 'body': '', 'sender': 2, 'style': 'bot' } } }
+        msg_json = { 'data': { 'attributes': { 'body': '', 'sender': 2, 'style': 'bot' } } }
         msg_json['data']['attributes']['body'] = message_text
         message_url = str(self.url) + "/api/messages"
         return requests.post(message_url, json=msg_json)
